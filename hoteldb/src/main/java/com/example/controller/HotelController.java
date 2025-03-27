fc0e06f05382cc2b19b71f8170ee4baf9fedb4bc
DanniFV <gitignore>
2025-03-27 17:18:53 +0000

    HotelController nokkrir hlutir komnir

--- hoteldb/src/main/java/com/example/controller/HotelController.java
+++ hoteldb/src/main/java/com/example/controller/HotelController.java
@@ -1,5 +1,44 @@
 package com.example.controller;
+import java.util.*;
+import java.util.List;
 
 public class HotelController {
-    
+
+    public HotelController {
+
+    }
+
+    public List<Hotel> searchHotelByName(String name) {
+
+    }
+
+    public List<Hotel> searchHotelByRegion() {
+
+    }
+
+    public Hotel getHotelInfo() {
+
+    }
+
+    public boolean addBooking() {
+
+    }
+
+    public boolean cancelBooking(){
+
+    }
+
+    public boolean updateAmenities(){
+
+    }
+
+    public boolean updateAvailableRooms(){
+
+    }
+
+    public boolean checkRoomAvailability(){
+
+    }
+
+
 }
