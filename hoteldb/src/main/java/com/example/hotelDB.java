75634969ed4ab09ea7dfe204fc2c66c39dcaa83e
Solvi Hrafn <shs89@hi.is>
2025-03-28 17:46:19 +0000

    eitthvað cool

---
+++ hoteldb/src/main/java/com/example/hotelDB.java
@@ -0,0 +1,21 @@
+package com.example;
+
+public class hotelDB {
+    
+    public boolean saveHotel(Hotel hotel){
+
+    }
+     public boolean updateHotel(Hotel hotel){
+
+    }
+    public boolean updateAvailableRooms(int hotelID, int rooms){
+
+    }
+    public hotel getHotel(int hotelID){
+
+    }
+     public List<Hotel> getHotelsByRegion(string region){
+
+     }
+     
+}
