71630acebe45bc24ab30533c87ef7ea4148e5115
blaakornblom <yih2@hi.is>
2025-03-30 11:01:53 +0000

    breytti Booking klasa

--- hoteldb/src/main/java/com/example/Booking.java
+++ hoteldb/src/main/java/com/example/Booking.java
@@ -1,17 +1,38 @@
 package com.example;
+import java.util.Date;
 
 public class Booking {
     private int hotelId;
     private int bookingId;
     private Date from;
     private Date to;
-    private guestName;
+    private String guestName;
 
-    public void getProperty() {
-
+    public Booking (int hotelId, int bookingId, Date from, Date to, String guestName) {
+        this.hotelId = hotelId;
+        this.bookingId = bookingId;
+        this.from = from;
+        this.to = to;
+        this.guestName = guestName;
     }
 
-    public void setProperty() {
+    public int getHotelId() {
+        return hotelId;
+    }
 
+    public int getBookingId() {
+        return bookingId;
+    }
+
+    public Date getFrom() {
+        return from;
+    }
+
+    public Date getTo() {
+        return to;
+    }
+
+    public String getGuestName() {
+        return guestName;
     }
 }
