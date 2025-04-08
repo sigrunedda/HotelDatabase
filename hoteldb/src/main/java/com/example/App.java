596cdb0b114a8520de2960a54c5f206427af98de
sigrunedda <sem24@hi.is>
2025-04-08 15:16:06 +0000

    eh

--- hoteldb/src/main/java/com/example/App.java
+++ hoteldb/src/main/java/com/example/App.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         System.out.print("Enter hotel ID: ");
         int hotelId = scanner.nextInt();
 
